7658ad0de9736d513b3185f0253748c42a155a73
Edward Lynch-Milner <eddylynch9@gmail.com>
2022-03-04 13:15:59 +0000

    Replaced proxying with an interceptor for production use

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   requireULEmail: true,
   testing: false,
   default_template_id: 'expedited',
-  login_redirect_token_expiry: 600000 // if / is navigated to and the authentication token is going to expire in X milliseconds, clear it and force re-login. This ensures token won't expire while filling in an application
+  login_redirect_token_expiry: 600000, // if / is navigated to and the authentication token is going to expire in X milliseconds, clear it and force re-login. This ensures token won't expire while filling in an application
+  api_base: 'https://ul-scieng-ethics-backend.herokuapp.com:8080'
 };
